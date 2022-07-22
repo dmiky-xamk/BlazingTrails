6d4b7dde92a7d2a558fe93f0b52e3bd3bc998e41
dmiky-xamk <dmiky001@edu.xamk.fi>
2022-07-22 21:40:49 +0300

    End of Chapter 2

---
+++ BlazingTrails.Client/Features/Home/Trail.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazingTrails.Client.Features.Home;
+
+public class Trail
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Desciption { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public int TimeInMinutes { get; set; }
+    public int Length { get; set; }
+    public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+
+    public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+}
+
+public class RouteInstruction
+{
+    public int Stage { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
