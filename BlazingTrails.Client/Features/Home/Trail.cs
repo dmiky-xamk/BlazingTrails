2e21eedae63a93b56d00f3042d252ddcd6962b89
dmiky-xamk <dmiky001@edu.xamk.fi>
2022-07-24 08:32:58 +0300

    End of Chapter 3

--- BlazingTrails.Client/Features/Home/Trail.cs
+++ BlazingTrails.Client/Features/Home/Trail.cs
@@ -4,7 +4,7 @@ public class Trail
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Desciption { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
     public int TimeInMinutes { get; set; }
