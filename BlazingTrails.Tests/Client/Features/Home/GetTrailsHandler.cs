00ae7e7839afda8c3024293486be35531960813e
dmiky-xamk <dmiky001@edu.xamk.fi>
2022-11-26 14:16:55 +0200

    End of Chapter 11

---
+++ BlazingTrails.Tests/Client/Features/Home/GetTrailsHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture;
+using BlazingTrails.Shared.Features.Home.Shared;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Tests.Client.Features.Home;
+
+// Test stub.
+public class GetTrailsHandler : IRequestHandler<GetTrailsRequest, GetTrailsRequest.Response>
+{
+    public async Task<GetTrailsRequest.Response> Handle(GetTrailsRequest request, CancellationToken cancellationToken)
+    {
+        var fixture = new Fixture();
+
+        // 'AutoFixture' is used to create a list of dummy trails to return.
+        var dummyTrails = fixture.CreateMany<GetTrailsRequest.Trail>();
+
+        return new GetTrailsRequest.Response(dummyTrails);
+    }
+}
